c8f700e200cc360bad7c0efc934c4a40649d2881
Bo Tang <lucastang1994@gmail.com>
2020-05-26 20:11:55 -0400

    Update to L2 project

--- src/TrafficObject.cpp
+++ src/TrafficObject.cpp
@@ -26,6 +26,8 @@ TrafficObject::TrafficObject()
 
 TrafficObject::~TrafficObject()
 {
-    // Set up a thread barrier that ensures that all the thread objects in the member vector _threads are joined.
-    std::for_each(_threads.begin(), _threads.end(), [](std::thread &t){t.join();});
+    // set up thread barrier before this object is destroyed
+    std::for_each(threads.begin(), threads.end(), [](std::thread &t) {
+        t.join();
+    });
 }
