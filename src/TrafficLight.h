5dd4a03dca33773797afa83c3953e244de7c770f
Bo Tang <lucastang1994@gmail.com>
2020-06-03 20:05:30 -0400

    Task FP.1 : Define class TrafficLight

--- src/TrafficLight.h
+++ src/TrafficLight.h
@@ -22,30 +22,33 @@ public:
 
 private:
     
-};
+}; 
 
-// FP.1 : Define a class „TrafficLight“ which is a child class of TrafficObject. 
-// The class shall have the public methods „void waitForGreen()“ and „void simulate()“ 
-// as well as „TrafficLightPhase getCurrentPhase()“, where TrafficLightPhase is an enum that 
-// can be either „red“ or „green“. Also, add the private method „void cycleThroughPhases()“. 
-// Furthermore, there shall be the private member _currentPhase which can take „red“ or „green“ as its value. 
+enum class TrafficLightPhase {red, green};
 
-class TrafficLight
+class TrafficLight : TrafficObject
 {
 public:
     // constructor / desctructor
+    TrafficLight();
+    ~TrafficLight();
 
     // getters / setters
+    TrafficLightPhase getCurrentPhase();
 
     // typical behaviour methods
+    void waitForGreen();
+    void simulate();
 
 private:
     // typical behaviour methods
-
+    void cycleThroughPhases();
+    
     // FP.4b : create a private member of type MessageQueue for messages of type TrafficLightPhase 
     // and use it within the infinite loop to push each new TrafficLightPhase into it by calling 
     // send in conjunction with move semantics.
-
+    
+    TrafficLightPhase _currentPhase;
     std::condition_variable _condition;
     std::mutex _mutex;
 };
